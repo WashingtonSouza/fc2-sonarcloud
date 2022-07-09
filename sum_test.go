c10711ff009c681b68bda42bafce9d095c5a5f1e
Washington Souza <thewashington.souza@gmail.com>
2022-07-09 17:40:47 +0100

    feat: add sum function

---
+++ sum_test.go
@@ -0,0 +1,13 @@
+package main
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	result := sum(2, 3)
+	if result != 5 {
+		t.Error("The result must be 5")
+	}
+}
+
+// run go test -> go test
+// generate cover.out -> go test -coverprofile=coverage.out
